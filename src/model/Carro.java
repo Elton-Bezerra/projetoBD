3cb6ba91d24e9283e88e506aeec1143749864121
Elton Bezerra <Elton Bezerra@192.168.0.14>
2017-05-27 20:34:29 -0300

    Arrumando a classe modelo Carro

--- src/model/Carro.java
+++ src/model/Carro.java
@@ -3,7 +3,36 @@ import java.util.Date;
 
 public class Carro {
 
+	private int id;
 	private char nome;
 	private Date ano;
+	private Montadora montadora;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public char getNome() {
+		return nome;
+	}
+	public void setNome(char nome) {
+		this.nome = nome;
+	}
+	public Date getAno() {
+		return ano;
+	}
+	public void setAno(Date ano) {
+		this.ano = ano;
+	}
+	public Montadora getMontadora() {
+		return montadora;
+	}
+	public void setMontadora(Montadora montadora) {
+		this.montadora = montadora;
+	}
+	
+	
 
 }
