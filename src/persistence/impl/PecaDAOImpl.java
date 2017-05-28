915ab498b19df1f5832e50447a12d39b9d3e7fb7
Elton Bezerra <Elton Bezerra@192.168.0.14>
2017-05-27 22:20:26 -0300

    Adicionando a classe PecaDAOImpl, hora de uma pausa pro café '-'

---
+++ src/persistence/impl/PecaDAOImpl.java
@@ -0,0 +1,39 @@
+package persistence.impl;
+
+import java.util.List;
+
+import model.Peca;
+
+public class PecaDAOImpl implements IDAOImpl<Peca>{
+
+	@Override
+	public void insert(Peca classe) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Peca classe) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Peca classe) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Peca searchById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Peca> listarTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
