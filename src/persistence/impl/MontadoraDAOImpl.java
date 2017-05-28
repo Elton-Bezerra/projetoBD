52a060aac20197fd48f0a9518201c71e91aaaf23
Elton Bezerra <Elton Bezerra@192.168.0.14>
2017-05-27 21:57:43 -0300

    Implementando todos os métodos na classe MontadoraDAOImpl

--- src/persistence/impl/MontadoraDAOImpl.java
+++ src/persistence/impl/MontadoraDAOImpl.java
@@ -13,26 +13,61 @@ public class MontadoraDAOImpl implements IDAOImpl<Montadora>{
 
 	GenericDAO gd;
 	Connection con;
-	
+	private List<Montadora> list;
 	public MontadoraDAOImpl(){
 		con = gd.getConnection();
 	}
 	@Override
 	public void insert(Montadora classe) {
-		// TODO Auto-generated method stub
+		String sql = "insert into Montadora values (?, ?)";
+		try {
+			PreparedStatement stmt = con.prepareStatement(sql);
+			stmt.setString(1, classe.getNome());
+			stmt.setString(2, classe.getDescricao());
+			
+			if(stmt.executeUpdate() > 0){
+				System.out.println("Montadora adicionada com sucesso");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
 	}
 
 	@Override
 	public void update(Montadora classe) {
-		// TODO Auto-generated method stub
+		String sql = "update Montadora set "
+				+ "nome = ?, descricao = ? where id = ?";
+		
+		try {
+			PreparedStatement stmt = con.prepareStatement(sql);
+			stmt.setString(1, classe.getNome());
+			stmt.setString(2, classe.getDescricao());
+			stmt.setInt(3, classe.getId());
+			if(stmt.executeUpdate() > 0){
+				System.out.println("Montadora atualizada com sucesso");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
 	@Override
 	public void delete(Montadora classe) {
-		// TODO Auto-generated method stub
+		String sql = "delete Montadora where id = ? ";
 		
+		try {
+			PreparedStatement stmt = con.prepareStatement(sql);
+			stmt.setInt(1, classe.getId());
+			if(stmt.executeUpdate() > 0){
+				System.out.println("Montadora deletada com sucesso");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Override
@@ -59,6 +94,22 @@ public class MontadoraDAOImpl implements IDAOImpl<Montadora>{
 	@Override
 	public List<Montadora> listarTodos() {
 		// TODO Auto-generated method stub
+		String sql = "SELECT * FROM Montadora";
+		try {
+			PreparedStatement stmt = con.prepareStatement(sql);
+			ResultSet rs = stmt.executeQuery();
+			list.clear();
+			while(rs.next()){
+				Montadora m = new Montadora();
+				m.setId(rs.getInt("id"));
+				m.setNome(rs.getString("nome"));
+				m.setDescricao(rs.getString("descricao"));
+				list.add(m);
+			}
+			return list;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 
